c9da36d16505b3b518c348e9bc8997cd7bb96d5b
Hamza Haque <hamzahaque30@gmail.com>
2024-03-11 09:30:29 -0500

    Add project files.

---
+++ CourseProject-CommandLineDBManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseProject_CommandLineDBManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
