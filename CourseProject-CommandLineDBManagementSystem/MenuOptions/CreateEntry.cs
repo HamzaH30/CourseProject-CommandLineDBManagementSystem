d3c8f6cac469b7608a2d8e4655d5bf9694916dac
Hamza Haque <hamzahaque30@gmail.com>
2024-03-11 15:30:52 -0500

    to continue

---
+++ CourseProject-CommandLineDBManagementSystem/MenuOptions/CreateEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MenuClasses;
+
+namespace CourseProject_CommandLineDBManagementSystem.MenuOptions
+{
+    public class CreateEntry : IMenuItem
+    {
+        public string SelectionText { get; init; }
+        public bool continueToCreateEntryMenu;
+
+        public CreateEntry()
+        {
+            SelectionText = "Create a new entry for a table";
+            continueToCreateEntryMenu = false;
+        }
+
+        public void PerformMenuAction()
+        {
+            continueToCreateEntryMenu = true;
+        }
+    }
+}
--- CourseProject-CommandLineDBManagementSystem/Program.cs
+++ CourseProject-CommandLineDBManagementSystem/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace CourseProject_CommandLineDBManagementSystem
+﻿using CourseProject_CommandLineDBManagementSystem.MenuOptions;
+using MenuClasses;
+
+namespace CourseProject_CommandLineDBManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Menu startingMenu = new Menu("Welcome to the Console-Based DBMS", "Choose one of the following options:");
+            startingMenu.AddToMenu(new CreateEntry());
+
+
         }
     }
 }
