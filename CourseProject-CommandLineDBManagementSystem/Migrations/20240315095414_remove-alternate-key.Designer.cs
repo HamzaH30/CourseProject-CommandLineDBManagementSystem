697c31b2bfc810d06a5536253689e83f174bb436
Hamza Haque <hamzahaque30@gmail.com>
2024-03-15 04:56:15 -0500

    remove alternate key

--- CourseProject-CommandLineDBManagementSystem/Migrations/20240315095414_remove-alternate-key.Designer.cs
+++ CourseProject-CommandLineDBManagementSystem/Migrations/20240315095414_remove-alternate-key.Designer.cs
@@ -4,6 +4,7 @@ using CourseProject_CommandLineDBManagementSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CourseProject_CommandLineDBManagementSystem.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240315095414_remove-alternate-key")]
+    partial class removealternatekey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CourseProject-CommandLineDBManagementSystem/Migrations/20240315095414_remove-alternate-key.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseProject_CommandLineDBManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class removealternatekey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Positions_Name",
+                table: "Positions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Positions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Positions",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Positions_Name",
+                table: "Positions",
+                column: "Name");
+        }
+    }
+}
